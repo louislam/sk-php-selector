4659f98aaf7a4a1e9b39213af096b9dd895cafc8
Louis Lam <louislam@users.noreply.github.com>
2017-11-10 13:16:53 +0800

    Update and rename sk-php-selector2.sh to install.sh

--- install.sh
+++ install.sh
@@ -27,7 +27,7 @@ exit 4
 fi
 
 fixit () {
-curl -s http://mirror.skamasle.com/vestacp/PHP/sk-php${1}-centos.sh > /usr/local/vesta/data/templates/web/httpd/sk-php${1}.sh
+curl -s https://raw.githubusercontent.com/louislam/vestacp-php-selector/master/scripts/sk-php${1}-centos.sh > /usr/local/vesta/data/templates/web/httpd/sk-php${1}.sh
 
 ln -s /usr/local/vesta/data/templates/web/httpd/phpfcgid.stpl /usr/local/vesta/data/templates/web/httpd/sk-php${1}.stpl
 
